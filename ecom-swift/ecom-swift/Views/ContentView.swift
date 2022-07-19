e530a0972d1483a59757cc131050041bac81be0f
Jason Louv <jason@magick.me>
2022-07-18 20:36:27 -0500

    Finished

--- ecom-swift/ecom-swift/Views/ContentView.swift
+++ ecom-swift/ecom-swift/Views/ContentView.swift
@@ -15,15 +15,18 @@ struct ContentView: View {
           .font(.title)
           .padding()
         Spacer()
-
         NavigationLink(
-            destination: ItemDetailView(itemName: "Shrimp Chips"),
-            label: {
-                Text("Shrimp Chips")
-                
-            })
+          destination: ItemDetailView(itemName: "Shrimp Chips"),
+          label: {
+            Text("Shrimp Chips")
+          })
         Spacer()
-    }}
+      }
+      // Sets a navigation title of "Ligaya's Store"
+      .navigationTitle(Text("Ligaya's Store"))
+      // Sets the styling of the navigation title to inline
+      .navigationBarTitleDisplayMode(.inline)
+    }
   }
 }
 
--- ecom-swift/ecom-swift/Views/ItemDetailView.swift
+++ ecom-swift/ecom-swift/Views/ItemDetailView.swift
@@ -9,6 +9,7 @@ import Foundation
 import SwiftUI
 
 struct ItemDetailView: View {
+  @State var quantityRemaining = Int.random(in: 1...10)
   let itemName: String
  
   var body: some View {
@@ -20,15 +21,24 @@ struct ItemDetailView: View {
       Image(systemName: "photo")
         .font(.system(size: 200))
         .padding()
-      Text("Quantity Remaining: 3")
+      Text("Quantity Remaining: \(quantityRemaining)")
       Spacer()
       Button {
-        print("Button tapped")
+        if quantityRemaining > 0 {
+          quantityRemaining -= 1
+        }
       } label: {
         Text("Add one to your cart")
       }
       Spacer()
     }
+    // .background() runs the view below in the background
+    .background(
+      // The navigation link is not active until the quantity is 0
+      NavigationLink(destination: Text("You bought all the \(itemName)!"),
+                     isActive: .constant(quantityRemaining == 0),
+                     label: { EmptyView() })
+    )
   }
 }
  
